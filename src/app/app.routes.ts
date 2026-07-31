dc06fca7c32dd8c7748669a6b82f12abe6abf4da
Vlad Beltran <puentes.studio@gmail.com>
2026-07-31 15:23:41 +0200

    routing splash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SplashComponent } from './splash/splash.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SplashComponent,
+  },
+];
